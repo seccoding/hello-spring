4532730f58dd09533339277aa0a7d59f82474a9e
Min Chang Jang <mcjang1116@gmail.com>
2024-04-05 13:01:45 +0900

    css, commonjs 추가

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -5,6 +5,21 @@ $().ready(function () {
       location.href = next;
     });
   });
+
+  /*
+   * 엔터키 눌렀을 때 form 전송안되도록 수정.
+   * input의 data-submit 값이 true가 아닌 경우, 엔터키입력시 전송 안되도록 방지.
+   */
+  $("form")
+    .find("input")
+    .on("keydown", function (event) {
+      if (event.keyCode === 13) {
+		var noSubmit = $(this).data("no-submit");
+		if (noSubmit !== undefined) {
+	        event.preventDefault();
+		}
+      }
+    });
 });
 
 function search(pageNo) {
