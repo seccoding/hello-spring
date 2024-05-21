7ed5d994983d69fe2a9d60777a6397a457c375c7
Min Chang Jang <mcjang1116@gmail.com>
2024-05-21 11:14:54 +0900

    React 연동 - 게시글 조회

--- src/main/java/com/hello/forum/bbs/api/ApiBoardController.java
+++ src/main/java/com/hello/forum/bbs/api/ApiBoardController.java
@@ -2,11 +2,13 @@ package com.hello.forum.bbs.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hello.forum.bbs.service.BoardService;
 import com.hello.forum.bbs.vo.BoardListVO;
+import com.hello.forum.bbs.vo.BoardVO;
 import com.hello.forum.bbs.vo.SearchBoardVO;
 import com.hello.forum.utils.ApiResponse;
 
@@ -24,6 +26,12 @@ public class ApiBoardController {
 
 		return ApiResponse.OK(boardListVO.getBoardList(),
 				boardListVO.getBoardCnt(), 1, false);
+	}
+
+	@GetMapping("/boards/{id}")
+	public ApiResponse getBoard(@PathVariable int id) {
+		BoardVO boardVO = this.boardService.getOneBoard(id, true);
+		return ApiResponse.OK(boardVO, boardVO == null ? 0 : 1);
 	}
 
 }
